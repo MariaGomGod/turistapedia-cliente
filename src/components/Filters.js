44a309a0f1abe2de2c1dff4dfe3b3be36a4f4cc4
Maria Gomez <maria_gg86@yahoo.es>
2021-05-31 21:18:24 +0200

    filtro accesibilidad finalizado

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,8 +1,8 @@
 import './Filters.sass';
 
-export default function Filters({ setCategories }) {
+export default function Filters({ setCategories, setAccessible }) {
 
-    function handleClick(e) {
+    function handleCategoryClicked(e) {
         const input = e.target;
         if (input.checked) {
             setCategories(currentCategories => [...currentCategories, input.value]);
@@ -11,30 +11,43 @@ export default function Filters({ setCategories }) {
         }
     }
 
+    function handleAccessibleClicked(e) {
+        const input = e.target;
+        if (input.checked) {
+            setAccessible(currentAccessible => [...currentAccessible, input.value]);
+        } else {
+            setAccessible(currentAccessible => currentAccessible.filter(accessible => accessible !== input.value));
+        }
+    }
+
     return (
         <div id="filters">
-            <div id="filter1">
-                <h3>Filtrar&nbsp;por&nbsp;categorías</h3>
-                <input type="checkbox" id="category-1" value="restauración" onChange={handleClick} defaultChecked={true}></input>
-                <label htmlFor="category-1">&nbsp;Restauración</label><br></br>
-                <input type="checkbox" id="category-2" value="alojamiento" onChange={handleClick} defaultChecked={true}></input>
-                <label htmlFor="category-2">&nbsp;Alojamiento</label><br></br>
-                <input type="checkbox" id="category-3" value="puente" onChange={handleClick} defaultChecked={true}></input>
-                <label htmlFor="category-3">&nbsp;Puente</label><br></br>
-                <input type="checkbox" id="category-4" value="plaza" onChange={handleClick} defaultChecked={true}></input>
-                <label htmlFor="category-4">&nbsp;Plaza</label>
-            </div>
+            <div class="border-section">
+                <div id="filter-wrapper">
+                    <div className="filter">
+                        <h3>Filtrar&nbsp;por&nbsp;categorías</h3>
+                        <input type="checkbox" id="category-1" value="restauración" onChange={handleCategoryClicked} defaultChecked={true}></input>
+                        <label htmlFor="category-1">Restauración</label><br></br>
+                        <input type="checkbox" id="category-2" value="alojamiento" onChange={handleCategoryClicked} defaultChecked={true}></input>
+                        <label htmlFor="category-2">Alojamiento</label><br></br>
+                        <input type="checkbox" id="category-3" value="puente" onChange={handleCategoryClicked} defaultChecked={true}></input>
+                        <label htmlFor="category-3">Puente</label><br></br>
+                        <input type="checkbox" id="category-4" value="plaza" onChange={handleCategoryClicked} defaultChecked={true}></input>
+                        <label htmlFor="category-4">Plaza</label>
+                    </div>
 
-            <div id="filter2">
-                <h3>Filtrar&nbsp;por&nbsp;accesibilidad</h3>
-                <input type="checkbox" id="accesible-1" value="sanitarios adaptados" defaultChecked={true}></input>
-                <label htmlFor="accesible-1">&nbsp;Sanitarios&nbsp;adaptados</label><br></br>
-                <input type="checkbox" id="accesible-2" value="acceso adaptado" defaultChecked={true}></input>
-                <label htmlFor="accesible-2">&nbsp;Acceso&nbsp;adaptado</label><br></br>
-                <input type="checkbox" id="accesible-3" value="habitación adaptada" defaultChecked={true}></input>
-                <label htmlFor="accesible-3">&nbsp;Habitación&nbsp;adaptada</label><br></br>
-                <input type="checkbox" id="accesible-4" value="audio guía" defaultChecked={true}></input>
-                <label htmlFor="accesible-4">&nbsp;Audio&nbsp;guía</label>
+                    <div className="filter">
+                        <h3>Filtrar&nbsp;por&nbsp;accesibilidad</h3>
+                        <input type="checkbox" id="accesible-1" value="adaptedToilet" onChange={handleAccessibleClicked}></input>
+                        <label htmlFor="accesible-1">Sanitarios&nbsp;adaptados</label><br></br>
+                        <input type="checkbox" id="accesible-2" value="adaptedAccess" onChange={handleAccessibleClicked}></input>
+                        <label htmlFor="accesible-2">Acceso&nbsp;adaptado</label><br></br>
+                        <input type="checkbox" id="accesible-3" value="adaptedRoom" onChange={handleAccessibleClicked}></input>
+                        <label htmlFor="accesible-3">Habitación&nbsp;adaptada</label><br></br>
+                        <input type="checkbox" id="accesible-4" value="audioGuide" onChange={handleAccessibleClicked}></input>
+                        <label htmlFor="accesible-4">Audio&nbsp;guía</label>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/pages/StreetMap.js
+++ src/pages/StreetMap.js
@@ -6,21 +6,26 @@ export default function StreetMap() {
 
   const [pointsOfInterest, setPointsOfInterest] = useState([]);
   const [center, setCenter] = useState({ lat: 37.3873069, lng: -6.0029337 });
-  const [categories, setCategories] = useState(["restauración", "alojamiento", "plaza", "puente"]);
+  const [categories, setCategories] = useState(["restauración", "alojamiento", "plaza", "puente"]); 
+  // Partimos de un array con todas las categorías. Añadir una categoría hace que potencialmente se muestren más puntos de interés.
+  const [accessible, setAccessible] = useState([]);
+  // Partimos de un array vacío, pues añadir un tipo de accesibilidad hace que potencialmente se muestren menos puntos de interés al ser más restrictivo.
+  // Por tanto, el escenario en el que potencialmente más puntos de interés se mostrarán, es aquel en el que están seleccionadas todas las categorías y
+  // no hay ningún filtro de accesibilidad seleccionado.
 
   useEffect(() => {
     window.speechSynthesis.cancel();
   }, []);
 
   useEffect(() => {
-    fetch(`http://localhost:8080/poi?latitude=${center.lat}&longitude=${center.lng}&categories=${categories}`)
+    fetch(`http://localhost:8080/poi?latitude=${center.lat}&longitude=${center.lng}&categories=${categories}&accessible=${accessible}`)
       .then(response => response.json())
       .then(data => setPointsOfInterest(data));
-  }, [center, categories]);
+  }, [center, categories, accessible]);
 
   return (
     <>
-      <Filters categories={categories} setCategories={setCategories} />
+      <Filters categories={categories} setCategories={setCategories} setAccessible={setAccessible} />
       <Map pointsOfInterest={pointsOfInterest} center={center} setCenter={setCenter} />
     </>);
 }
