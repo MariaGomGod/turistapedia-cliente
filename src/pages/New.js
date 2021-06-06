2e17a47aebd5ffe27d068ad856bc4f39096ed26b
Maria Gomez <maria_gg86@yahoo.es>
2021-06-06 19:55:18 +0200

    validación de categorías desde el front end

--- src/pages/New.js
+++ src/pages/New.js
@@ -75,6 +75,24 @@ export default function New() {
         });
     }
 
+    function updateCategories(e) {
+
+        updateCheckboxField(e, "categories");
+
+        const inputs = document.querySelectorAll('div#categories-checkbox input[type="checkbox"]');
+
+        let atLeastOneChecked = false;
+
+        for (let i = 0; i < inputs.length; i++) {
+            if (inputs[i].checked) {
+                atLeastOneChecked = true;
+                break;
+            }
+        }
+
+        inputs.forEach(element => element.required = !atLeastOneChecked);
+    }
+
     return (
         <div id="new-poi">
             <NotificationContainer />
@@ -111,43 +129,43 @@ export default function New() {
                     <div id="categories-checkbox" className="form-group checkbox">
                         <div className="control">
                             <label htmlFor="category-1">Restauración</label>
-                            <input type="checkbox" id="category-1" value="restauración" onChange={e => updateCheckboxField(e, "categories")}></input>
+                            <input type="checkbox" id="category-1" value="restauración" onChange={updateCategories} required></input>
                         </div>
                         <div className="control">
                             <label htmlFor="category-2">Bar</label>
-                            <input type="checkbox" id="category-2" value="bar" onChange={e => updateCheckboxField(e, "categories")}></input>
+                            <input type="checkbox" id="category-2" value="bar" onChange={updateCategories} required></input>
                         </div>
                         <div className="control">
                             <label htmlFor="category-3">Restaurante</label>
-                            <input type="checkbox" id="category-3" value="restaurante" onChange={e => updateCheckboxField(e, "categories")}></input>
+                            <input type="checkbox" id="category-3" value="restaurante" onChange={updateCategories} required></input>
                         </div>
                         <div className="control">
                             <label htmlFor="category-4">Alojamiento</label>
-                            <input type="checkbox" id="category-4" value="alojamiento" onChange={e => updateCheckboxField(e, "categories")}></input>
+                            <input type="checkbox" id="category-4" value="alojamiento" onChange={updateCategories} required></input>
                         </div>
                         <div className="control">
                             <label htmlFor="category-5">Hotel</label>
-                            <input type="checkbox" id="category-5" value="hotel" onChange={e => updateCheckboxField(e, "categories")}></input>
+                            <input type="checkbox" id="category-5" value="hotel" onChange={updateCategories} required></input>
                         </div>
                         <div className="control">
                             <label htmlFor="category-6">Hostal</label>
-                            <input type="checkbox" id="category-6" value="hostal" onChange={e => updateCheckboxField(e, "categories")}></input>
+                            <input type="checkbox" id="category-6" value="hostal" onChange={updateCategories} required></input>
                         </div>
                         <div className="control">
                             <label htmlFor="category-7">Apartamento</label>
-                            <input type="checkbox" id="category-7" value="apartamento" onChange={e => updateCheckboxField(e, "categories")}></input>
+                            <input type="checkbox" id="category-7" value="apartamento" onChange={updateCategories} required></input>
                         </div>
                         <div className="control">
                             <label htmlFor="category-8">Monumento</label>
-                            <input type="checkbox" id="category-8" value="monumento" onChange={e => updateCheckboxField(e, "categories")}></input>
+                            <input type="checkbox" id="category-8" value="monumento" onChange={updateCategories} required></input>
                         </div>
                         <div className="control">
                             <label htmlFor="category-9">Puente</label>
-                            <input type="checkbox" id="category-9" value="puente" onChange={e => updateCheckboxField(e, "categories")}></input>
+                            <input type="checkbox" id="category-9" value="puente" onChange={updateCategories} required></input>
                         </div>
                         <div className="control">
                             <label htmlFor="category-10">Plaza</label>
-                            <input type="checkbox" id="category-10" value="plaza" onChange={e => updateCheckboxField(e, "categories")}></input>
+                            <input type="checkbox" id="category-10" value="plaza" onChange={updateCategories} required></input>
                         </div>
                     </div>
                 </div>
