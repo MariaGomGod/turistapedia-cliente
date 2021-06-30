f5d4c79a9926b9a2a6f75f96937f731a41e530af
Maria Gomez <maria_gg86@yahoo.es>
2021-06-30 16:05:07 +0200

    arreglada pagina de accesibilidad

--- src/pages/create/Accesibility.js
+++ src/pages/create/Accesibility.js
@@ -1,21 +1,12 @@
 export default function Accesibility({ pointOfInterest, setPointOfInterest }) {
 
-    function updateCheckboxField(e, field) {
-
-        // Creamos una función genérica para actualizar un campo de tipo array de strings, dado su nombre y el input que contiene su nuevo valor.
-        // Si el input está marcado, el nuevo valor se añade al campo, de lo contrario, se eliminará.
-
+    function updateAccessible(e) {
         const checked = e.target.checked;
         const input = e.target.value;
         setPointOfInterest(currentPointOfInterest => {
-            const newPointOfInterest = { ...currentPointOfInterest };
-            newPointOfInterest[field] = newPointOfInterest[field] || [];
-
-            if (checked) {
-                newPointOfInterest[field] = [...newPointOfInterest[field], input];
-            } else {
-                newPointOfInterest[field] = newPointOfInterest[field].filter(element => element !== input);
-            }
+            const newPointOfInterest = {...currentPointOfInterest};
+            newPointOfInterest.accessible = {...newPointOfInterest.accessible};
+            newPointOfInterest.accessible[input] = checked;
             return newPointOfInterest;
         });
     }
@@ -26,19 +17,19 @@ export default function Accesibility({ pointOfInterest, setPointOfInterest }) {
             <div id="accesible-checkbox" className="form-group checkbox">
                 <div className="control">
                     <label htmlFor="accesible-1">Sanitarios&nbsp;adaptados</label>
-                    <input type="checkbox" id="accesible-1" value="adaptedToilet" defaultChecked={pointOfInterest.accessible?.adaptedToilet} onInput={e => updateCheckboxField(e, "accessible")}></input><br />
+                    <input type="checkbox" id="accesible-1" value="adaptedToilet" defaultChecked={pointOfInterest.accessible?.adaptedToilet} onInput={updateAccessible}></input><br />
                 </div>
                 <div className="control">
                     <label htmlFor="accesible-2">Acceso&nbsp;adaptado</label>
-                    <input type="checkbox" id="accesible-2" value="adaptedAccess" defaultChecked={pointOfInterest.accessible?.adaptedAccess} onInput={e => updateCheckboxField(e, "accessible")}></input><br />
+                    <input type="checkbox" id="accesible-2" value="adaptedAccess" defaultChecked={pointOfInterest.accessible?.adaptedAccess} onInput={updateAccessible}></input><br />
                 </div>
                 <div className="control">
                     <label htmlFor="accesible-3">Habitaciones&nbsp;adaptadas</label>
-                    <input type="checkbox" id="accesible-3" value="adaptedRoom" defaultChecked={pointOfInterest.accessible?.adaptedRoom} onInput={e => updateCheckboxField(e, "accessible")}></input><br />
+                    <input type="checkbox" id="accesible-3" value="adaptedRoom" defaultChecked={pointOfInterest.accessible?.adaptedRoom} onInput={updateAccessible}></input><br />
                 </div>
                 <div className="control">
                     <label htmlFor="accesible-4">Audio&nbsp;guía</label>
-                    <input type="checkbox" id="accesible-4" value="audioGuide" defaultChecked={pointOfInterest.accessible?.audioGuide} onInput={e => updateCheckboxField(e, "accessible")}></input><br />
+                    <input type="checkbox" id="accesible-4" value="audioGuide" defaultChecked={pointOfInterest.accessible?.audioGuide} onInput={updateAccessible}></input><br />
                 </div>
             </div>
         </>
