c0f3ba038d71f2a79e8ba9ddb4b633f21160aab0
Maria Gomez <maria_gg86@yahoo.es>
2021-07-25 22:11:00 +0200

    Arregladas variables de entorno para backend

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
-const HOST = process.env.BACKEND_HOST || "http://localhost";
-const PORT = process.env.BACKEND_PORT || "8080";
+const HOST = process.env.REACT_APP_BACKEND_HOST || "http://localhost";
+const PORT = process.env.REACT_APP_BACKEND_PORT || "8080";
 const BASE_API_URL = HOST + ":" + PORT;
 
 export {
